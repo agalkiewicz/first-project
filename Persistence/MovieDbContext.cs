99dbea424952a8351f5e38a08e130486969719c5
alicja.galkiewicz <alicja.galkiewicz@gmail.com>
2026-02-17 16:17:56 +0100

    Add basic model and initial db configuration

---
+++ Persistence/MovieDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+public class MovieDbContext(DbContextOptions<MovieDbContext> options) : DbContext(options)
+{
+    public DbSet<Movie> Movies => Set<Movie>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("app");
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MovieDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
